792c959cff49c4d020f8982593c7ab13042541bb
Ivan Pereira <ivan.pereira@booking.com>
2019-09-11 23:26:10 +0200

    Simplify the code that generates the search highlights, move the logic to create the HTML of each line to the Line component

--- ui/assets/js/components/HoundApp/Line.jsx
+++ ui/assets/js/components/HoundApp/Line.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import {Model} from '../../helpers/Model';
+import { Model } from '../../helpers/Model';
+import { ContentFor } from '../../utils';
 
 export const Line = (props) => {
 
-    const { line, rev, repo, filename, content } = props;
+    const { line, rev, repo, filename, regexp } = props;
+    const content = ContentFor(line, regexp);
 
     return (
         <div className="line">
