ade0eabd508b96cf4a3e4aeeb84832903b447d80
Ivan Yelizariev <yelizariev@it-projects.info>
2020-01-19 14:31:50 +0000

    release workflow

---
+++ cd/deploy-uwsgi.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def application(env, start_response):
+    res = subprocess.run(['bash', 'deploy.sh'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    if res.returncode == 0:
+        code = '200 OK'
+    else:
+        code = '500 Error'
+    start_response(code, [('Content-Type','text/plain')])
+    return [res.stdout]
---
+++ cd/deploy.example.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+DOCKER=docker.pkg.github.com/it-projects-llc/hound/production
+NAME=hound
+DATA=$(pwd)
+docker pull $DOCKER
+docker stop $NAME
+docker rm $NAME
+docker run -d -p 6080:6080 --name $NAME -v $DATA:/data $DOCKER
